698b8ebc7d7b76d46c8a3604328e43fb78ce2955
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2023-09-19 15:23:41 +0000

    activity work functions Jordan Jones

--- Ap_quizzes/Quiz_One.py
+++ Ap_quizzes/Quiz_One.py
@@ -1,11 +1,17 @@
 # 1. In your on words, describe what a variable is.
- "A variable is a letter or symbol of text that is" 
-# 2. Name three (3) operators we've learned about in class and describe 
+ "A variable is a letter or symbol of text that is changable depending on the situation." 
+# 2. Name three (3) operators we've learned about in class and describe
+"Addition (+) is when you combine two numbers. creating a new bigger number, subtraction(-) is when you negatively compare two values. 
+"One value is subtracted into the other, leading to a smaller number. Multiplication(*) is when you multiply a number by another value. Example: 3x3 = 9 " 
 # a situation where you would use each one. 
-
+"I would use addition is when setting a table. You need two pieces of the table to make a bigger table."
+"I would use subtraction when using a motorcycle. I need to remove the damaged tire, therefore I am subtracting the two wheels by one."
+"I would use multiplication when finding the solution in an algebraic expression (It tells me that the variable is 4 times greater than the present given number."
 # 3. Describe the difference between camal case and pascal case.
-
+"A camal case is when every word starts with a capital letter after the second word of a variable name"
+"A Pascal case is when every word, including the first word, starts with a capital letter."
 # 4. Which operator is being used in the example below
+"The operator that is used is a variable"
 numberOfApples = 10
 numberOfOranges = 12
 
@@ -15,22 +21,36 @@ numberOfApples < numberOfOranges
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
-
+original_gift_card = 100 
+family_member = + 50
+friend = + 35.50
+total_amount = 185.50
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+name = input("What is your name?")
 
+print("My name is" + name)
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
 # They want to know a visitor's full name, their age, if they have an allergy, and what their 
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
+name = input("What is your name?")
+print("My name is" + name)
+age = input("What is your age?")
+print("I am" + age)
+allergy = input("What is your allergy")
+print("my allergy is" + allergy)
+food = input("What is you favorite food?")
+print("my favorite food is" + food) 
 
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
-
+"A good data type is Boolean. You can use it to write 'true or false' values to answer the allergy questions."
 # 9. In your own words, describe what a program is?
-
+"A program is a certain related system that is encoded to guide people using specific topics. Programs can include websites, or online articles."
 # 10. Describe the difference between a float and an integer. 
+"A float is a decimal number, while an integer is a whole number with no decimal."
