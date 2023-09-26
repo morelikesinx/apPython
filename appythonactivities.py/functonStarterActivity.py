8b0f89a7207f6689fda13a0313fbff51fd8ee8e2
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2023-09-26 14:09:13 +0000

    All done work JordanJones

--- appythonactivities.py/functonStarterActivity.py
+++ appythonactivities.py/functonStarterActivity.py
@@ -2,12 +2,12 @@
 "A function is a form of code that uses inputs and outputs to take in data and uses it for completing an online task."
 # 2. What is are function parameters and arguments and describe
 # the difference between the 2. 
-"A parameter is a variable listed in paranthesis. As argument is a value that is translated to the function once it is called."
+"A parameter is a variable listed in paranthesis - a placeholder. An argument is a value that is translated to the function once it is called."
 # 3. write a function that will print out a welcome message
 # that includes a users name. You will need to use parameters and arguments
 def WelcomeToWendys():
     print('Welcome to Wendys')
-    print('--------------------------------')
+    print('Have a nice day')
 print('Jordan')
 WelcomeToWendys()
 # 4. Write a function that will do a calculation that takes 3 parameters.
@@ -31,3 +31,10 @@ print("3.Multiply")
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
 # be real, user data- not hard coded data.
+
+def userMsg():
+    nextClass = input('what is your next class.')
+    print("you have " + nextClass +" after this.")
+
+
+userMsg()
