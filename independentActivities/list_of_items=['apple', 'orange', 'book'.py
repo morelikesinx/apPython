d27227a49472a54ea98e828227bb58c7baf40539
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2023-10-05 15:23:45 +0000

    tug of war activity testing

--- independentActivities/list_of_items=['apple', 'orange', 'book'.py
+++ independentActivities/list_of_items=['apple', 'orange', 'book'.py
@@ -1,14 +1,13 @@
-list_of_items=['apple', 'orange', 'book']
+data= input('input a data type ')
 
-apple_price= 1.00
-orange_price= 3.00
-book_price=10.00
+hi= type(data)
 
-def cart_items():
-    newItem_name= input('Enter name of new item')
-    newItem_price= float(input('Enter price of new item'))
-    list_of_items.append(newItem_name)
-    total_price= apple_price + orange_price + book_price + newItem_price
-    print(list_of_items)
-    print(total_price)
-cart_items()
+def datatype():
+    if hi == str(data):
+        print('That is a string')
+    elif hi == int(data):
+        print("that is an integer")
+    if hi == float(data):
+        print('That is a float')
+
+datatype()
---
+++ testing for functions.py
@@ -0,0 +1,6 @@
+numlist = [1,2,3,4,5,6]
+
+def muliply():
+    print(numlist* 3)
+
+muliply()
