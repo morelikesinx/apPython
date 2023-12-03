51127dfe4deb631ceb486567a8980c6cc91b02d6
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2023-12-03 21:55:52 +0000

    Updated changes for completion of project

---
+++ jordanj_python_TTT_Program.py/readME.md
@@ -0,0 +1,24 @@
+# Jordan's Tic Tac Toe
+
+# About 
+This project is an innovative Tic Tac Toe game coded in Python using the terminal. This game is very popular online and in boardgames.
+
+# Features
+Jordan's Tic Tac Toe has three (3) main features:
+1. Provides the user with two options of players to choose from: Player 1, or Player 2.
+2. The characters, X or O, can be placed on a 3x3 grid.
+3. This program allows the players to win in three ways: Horizontal, Vertial, or Diagnol.
+
+# Contributors
+This Tic Tac Toe Function was created by Jordan Jones from the AP Python class.
+
+# Project Review
+
+1. <b>What inspired you (or your team) to develop this program?</b>
+I was inspired to develop this program by seeing other tic tac toe programs being put into real games. Both online and actual boardgames.
+
+2. <b>What was a road block that you had with writing your program? How did you solve your  problem?</b>
+There were a few road blocks that came up during the development of this program. They were certain syntax problems halfway through the code that left me blocked for a few hours. The two other problems with issues with the positioning of X and O on the board, and the grid itself being uneven.
+
+3. <b>What is somthing you would have liked to have added or done differently?</b>
+If I had more time, I would have tried to re-configure the code into being played by the computer, therefore the player gets more of a challenge.
---
+++ jordanj_python_TTT_Program.py/tic_tac_toe_program.py
@@ -0,0 +1,108 @@
+#game board
+board = ["_","_","_",
+         "_","_","_",
+         "_","_","_"]
+
+#game indicator
+game_on = True
+
+# Initialize player
+current_player = "X"
+
+#Display game board
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2] + "    " + "1|2|3")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "    " + "4|5|6")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "    " + "7|8|9")
+#define players
+def players():
+    print("Select Player - X or O")
+    p1 = input("Player1: ")
+    p2 = ""
+    if p1 == "X":
+        p2 = "O"
+        print("Player2: " + p2)
+    elif p1 == "O":
+        p2 = "X"
+        print("Player2: " + p2)
+    elif p1 != "O" or p1 != "X":
+        print("Invalid. Type X or O")
+        play_game()
+
+#Player position
+def player_position():
+    global current_player
+    print("Current Player: " + current_player)
+    position = input("Choose position from 1 - 9: ")
+
+    # Loop until win or tie
+    valid = False
+    while not valid:
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+          position = input("Choose position from 1 - 9: ")
+        position = int(position) - 1
+
+        if board[position] == "_":
+            valid = True
+        else:
+            print("Position already selected, choose another position!")
+    board[position] = current_player
+    display_board()
+
+#play game function 
+def play_game():
+    print("Jordan's Tic Tac Toe Game")
+    display_board()
+    players()
+    
+  #flip players until there is a win
+    while game_on:
+        player_position()
+        
+        #Check winner
+        def check_winner():
+            global game_on
+            #Check rows for win
+            if board[0] == board[1] == board[2] != "_":
+                game_on = False
+                print("Congrats! " + board[0]+" you won!")
+            elif board[3] == board[4] == board[5] != "_":
+                game_on = False
+                print("Congrats! " + board[3]+" you won!")
+            elif board[6] == board[7] == board[8] != "_":
+                game_on = False
+                print("Congrats! " + board[6]+" you won!")
+             #Check columns for win
+            elif board[0] == board[3] == board[6] != "_":
+                game_on = False
+                print("Congrats! " + board[0]+" you won!")
+            elif board[1] == board[4] == board[7] != "_":
+                game_on = False
+                print("Congrats! " + board[1]+" you won!")
+            elif board[2] == board[5] == board[8] != "_":
+                game_on = False
+                print("Congrats! " + board[2]+" you won!")
+             #Check diagonals for win
+            elif board[0] == board[4] == board[8] != "_":
+                game_on = False
+                print("Congrats! " + board[0]+" you won!")
+            elif board[2] == board[4] == board[6] != "_":
+                game_on = False
+                print("Congrats! "+ board[6]+" you won!")
+             #If none of the above, its a tie
+            elif "_" not in board:
+                game_on = False
+                print("It's a Tie")
+                exit()
+
+        #flip player
+        def flip_player():
+            global current_player
+            if current_player == "X":
+                current_player = "O"
+            else:
+                current_player = "X"
+        flip_player()
+        check_winner()
+#Play the game
+play_game()
