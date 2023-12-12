3a6b25f72981b51f8183b4ab0ec95557bc866449
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2023-12-12 16:17:28 +0000

    commit for added social media document: finished

---
+++ appythonactivities.py/socialmediaprompt.py
@@ -0,0 +1,18 @@
+Watch and listen intently to the video and answer the following questions.
+Please write 1 paragraph (3-4 complete sentences). 
+
+1. How important is it for you to have a large social media following and or a lot of likes on your social media account?
+'Of my current life, I do not find it very important to have a lot of likes or large social media following.'
+'Fame shows some importance, but  not enough to where I would want an interview. If I would gather attention, I would'
+'rather it be small, not grobally large like most would want it.'
+
+2. Have you ever purchased somthing becuase you saw it on social media? If yes, what was it and why
+did you purhcase it? 
+'Yes, I have purchased a headphone known as the DT770, and I have brought it to school many times.'
+'It is very known for its big contributions and reputation for phenominal studio monitoring cabilities,'
+'as long as being a headphone that can bring you into the realm of HIFI audio.'
+'I bought them because my current headphone pair bored me, as the signature of the sound didnt please me as it used to.'
+'And so, I wanted to hear what actual good quality audio sounds like to the ears, and It was worth it'
+'Another thing that was purchased was from my parents is The Orion SkyQuest XT6. '
+'This is one of the very classic dobsonian Reflectors that are known worldwide for viewing the night sky,' 
+'especially for beginnner astronomers. A few others is the Apertura AD series, Orion Starblast, and Celestron NexStar series.'
