fad75958cf2d51d1485e63063458146ebc8ec22a
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2024-01-04 16:23:32 +0000

    changes to python review 1/4/24

--- Last Python Unit/pythonReview.py
+++ Last Python Unit/pythonReview.py
@@ -6,3 +6,41 @@
 # 3. I would use a string if I would want to make a list for a function
 
 # We use the input function to input a user's data
+
+# Strings 
+name = "Jordan"
+school = "Boys Latin"
+
+# Booleans
+IsTrue = False
+IsFalse = True
+
+# Integers
+age= 1
+backAccount = 2398429420384
+
+# floats
+backAccount = 2937.33
+
+# Lists
+groceryCart=['orange','apple','milk','water']
+lotteryNumbers=[10,10,38,6,456]
+listOfTodo=[groceryCart,lotteryNumbers]
+
+# Arithmatic -- >
+numberOne = int('4')
+
+numberTwo = int('20')
+
+answer = numberOne * numberTwo
+
+print(answer)
+
+# assignment operator
+numberofGuest_partyreservation=200
+
+# conparison operator
+valueA = 10
+valueB = 12
+
+whichIsGreater = valueA>valueB
