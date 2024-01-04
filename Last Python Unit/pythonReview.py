e1772f721ed093dda1a30be982f458b43c3301bd
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2024-01-04 15:26:09 +0000

    commit for python review 1/4/24

---
+++ Last Python Unit/pythonReview.py
@@ -0,0 +1,8 @@
+# A computer program is a block of code used as a set of instructions for the computer to do something.
+
+# Three data types include: an integer, boolean, and string.
+# 1. I would use an integer when I would want to input a number into a block of code.
+# 2. I would use a boolean when using a conditional function to determine of something is true or false
+# 3. I would use a string if I would want to make a list for a function
+
+# We use the input function to input a user's data
