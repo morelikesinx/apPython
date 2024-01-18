e53c1b2b6aab4e0884d1b35c0da5bdce149f1f1f
morelikesinx <144819530+morelikesinx@users.noreply.github.com>
2024-01-18 17:55:35 +0000

    Commit for 1/18/24 extra credit

---
+++ Last Python Unit/pythonreview_3.py
@@ -0,0 +1,6 @@
+def HowManyCharacters():
+    print("How many numbers does your words have? Enter them below:")
+    User_Characters = input("Enter words, characters, numbers, or phrases:")
+    print(len(User_Characters))
+    
+HowManyCharacters()
